78721809d483a83e7c22da223c3b54700eaa38f7
jonaswong300 <61023335+jonaswong300@users.noreply.github.com>
2022-09-04 00:01:46 +0800

    Angular 14 standalone component showcase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'lazy',
+  loadComponent: () => import('./parent-component/parent-component.component').then((mod)=> mod.ParentComponentComponent)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ParentComponentComponent } from './parent-component/parent-component.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [ParentComponentComponent],
+  standalone: true
 })
 export class AppComponent {
   title = 'angular-14';
